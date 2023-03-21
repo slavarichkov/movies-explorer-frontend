44860e189693f7fe649014c66d9d27adeaad02dc
Slava Richkov <v.v.richkov@yandex.ru>
2023-03-21 12:21:47 +0800

    fix: отступы

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,17 +8,17 @@ function AboutMe() {
         <section className="about-me">
             <h2 className="about-me__title">Студент</h2>
             <div className="about-me__conteiner-info">
-                <div className="about-me__conteiner-info__element">
-                    <h2 className="about-me__conteiner-info__element__name">Виталий</h2>
-                    <h3 className="about-me__conteiner-info__element__info">Фронтенд-разработчик, 30 лет</h3>
-                    <p className="about-me__conteiner-info__element__description">
+                <div className="about-me__description-elemets">
+                    <h2 className="about-me__name">Виталий</h2>
+                    <h3 className="about-me__info">Фронтенд-разработчик, 30 лет</h3>
+                    <p className="about-me__description">
                         Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
                         и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
                     </p>
-                    <a href='https://github.com/slavarichkov' className="about-me__conteiner-info__element__link" target="_blank" rel="noreferrer">Github</a>
+                    <a href='https://github.com/slavarichkov' className="about-me__link" target="_blank" rel="noreferrer">Github</a>
                 </div>
-                <div className="about-me__conteiner-info__element">
-                    <img src={photo} className="about-me__conteiner-info__element__photo" alt="фото студента" />
+                <div className="about-me__description-elemets">
+                    <img src={photo} className="about-me__photo" alt="фото студента" />
                 </div>
             </div>
         </section>

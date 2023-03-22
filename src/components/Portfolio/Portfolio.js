ae437f98c00dcda4bcd7f35455875d8019d5b67e
Slava Richkov <v.v.richkov@yandex.ru>
2023-03-22 13:34:30 +0800

    fix: отступы

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,20 +4,26 @@ function Portfolio() {
     return (
         <div className="portfolio">
             <h3 className="portfolio__title">Портфолио</h3>
-            <div className="portfolio__links">
-                <a href='https://slavarichkov.github.io/russian-travel/index.html' target='blanc' rel="noreferrer" className="portfolio__link" >
-                    <p className="portfolio__link-text">Статичный сайт</p>
-                    <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
-                </a>
-                <a href='https://slavarichkov.github.io/front-end-Mesto/' target='blanc' rel="noreferrer" className="portfolio__link">
-                    <p className="portfolio__link-text">Адаптивный сайт</p>
-                    <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
-                </a>
-                <a href='https://slavarichkov.github.io/front-end-Mesto/' target='blanc' rel="noreferrer" className="portfolio__link">
-                    <p className="portfolio__link-text">Одностраничное приложение</p>
-                    <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
-                </a>
-            </div>
+            <ul className="portfolio__links">
+                <li className="portfolio__list">
+                    <a href='https://slavarichkov.github.io/russian-travel/index.html' target='blanc' rel="noreferrer" className="portfolio__link" >
+                        <p className="portfolio__link-text">Статичный сайт</p>
+                        <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
+                    </a>
+                </li>
+                <li className="portfolio__list">
+                    <a href='https://slavarichkov.github.io/front-end-Mesto/' target='blanc' rel="noreferrer" className="portfolio__link">
+                        <p className="portfolio__link-text">Одностраничное приложение</p>
+                        <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
+                    </a>
+                </li>
+                <li className="portfolio__list">
+                    <a href='https://slavarichkov.github.io/front-end-Mesto/' target='blanc' rel="noreferrer" className="portfolio__link">
+                        <p className="portfolio__link-text">Адаптивный сайт</p>
+                        <img className="portfolio__link-image" src={link} alt="изображение ссылки" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
